5b691155408529a0b690e5c8f5d97f2fd7055f56
kimidorikame <kametubo@gmail.com>
2026-08-01 08:44:35 +0900

    NEWGAME時にGameStateを初期化（初期資金を定数化、翌日継続は維持）

--- autoload/game_state.gd
+++ autoload/game_state.gd
@@ -3,9 +3,10 @@ extends Node
 enum Phase { MORNING, MARKET, PREP, NIGHT, RESULT, HOME }
 
 const REP_ENDING_THRESHOLD := 5
+const INITIAL_MONEY := 3000
 
 var day: int = 1
-var money: int = 3000
+var money: int = INITIAL_MONEY
 var reputation: int = 0
 var inventory: Dictionary = {}
 var story_flags: Dictionary = {}
@@ -14,3 +15,13 @@ var phase: Phase = Phase.MORNING
 
 func next_day() -> void:
 	day += 1
+
+
+# NEWGAME開始時に呼ぶ。LOAD時はセーブデータで上書きするため呼ばない。
+func reset_for_new_game() -> void:
+	day = 1
+	money = INITIAL_MONEY
+	reputation = 0
+	inventory = {}
+	story_flags = {}
+	phase = Phase.MORNING
